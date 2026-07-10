267e66347888ce5256e42ed6a00841829fd92a76
Carlos Felipe <habbongz1@gmail.com>
2026-07-10 12:19:23 -0300

    Adicipnado keep_live

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import os
 from dotenv import load_dotenv
 
 from bot import bot
+from keep_alive import keep_alive
 
 
 async def main():
-    
+
     load_dotenv()
     TOKEN = os.getenv("DISCORD_TOKEN")
 
     if not TOKEN:
         print("Token não encontrado!")
         return
+    
+    keep_alive()
 
     async with bot:
         await bot.start(TOKEN)
